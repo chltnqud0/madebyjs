ece8c0941289421ab44cd392d8b4d0ae4f549df6
chltnqud0 <csb0328@gmail.com>
2023-07-28 21:35:18 +0900

    create update func

--- backend_nestjs/src/boards/boards.controller.ts
+++ backend_nestjs/src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
-import { Board } from './board.model';
+import { Patch } from '@nestjs/common/decorators';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -25,5 +26,13 @@ export class BoardsController {
   @Delete(':/id')
   deleteBoard(@Param('id') id: string): void {
     this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }

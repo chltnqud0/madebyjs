37ecb9fb13fae92c38edebec1dd062874c3b413e
chltnqud0 <csb0328@gmail.com>
2023-08-04 19:52:02 +0900

    create config

--- backend_nestjs/src/main.ts
+++ backend_nestjs/src/main.ts
@@ -1,12 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`application running at ${port}`);
+  const serverConfig = config.get('server');
+  await app.listen(serverConfig.port);
+  logger.log(`application running at ${serverConfig.port}`);
 }
 bootstrap();
